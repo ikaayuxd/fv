c6769a1ad4ddaec921ea74792270d7c4776683ec
Jhovkvj <117099782+ikaayuxd@users.noreply.github.com>
2024-09-27 18:27:02 +0530

    Update seen.py

--- seen.py
+++ seen.py
@@ -1,8 +1,8 @@
-import requests 
-import random 
-import sys 
-import threading 
-import psutil 
+import requests
+import random
+import sys
+import threading
+import psutil
 import os
 
 def read_proxies_from_file(file_path):
@@ -25,30 +25,31 @@ def http_start(link, proxies):
                 'user-agent': 'Mozilla/5.0 (Linux; Android 10; SM-G981B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Mobile Safari/537.36',
                 'x-requested-with': 'XMLHttpRequest'
             })
-            
+
             main_res = session.get(link)
             _token = main_res.text.split('data-view="')[1].split('"')[0]
             print("Extracted Token:", _token)
-            
+
             views_req = session.get(f"https://t.me/v/?views={_token}")
             print(' [+] View Sent ' + 'Status Code: ' + str(views_req.status_code) + ' Response: ' + views_req.text)
-            
+
             proxies.remove(proxy)
             req_count += 1
-        except Exception as e:
-            print("An exception occurred:", e)
+        except requests.exceptions.RequestException as e:
+            print("An exception occurred while sending the view:", e)
+            # Handle Proxy Authentication Required error here
 
 try:
     count = sys.argv[3]
     req_count = 0
     link = sys.argv[1].strip().replace('https://', '').replace('http://', '')
     url_fin = f'https://{link}?embed=1'
-    
+
     if sys.argv[2] == "file":
         proxies = read_proxies_from_file('proxies.txt')
         for ii in range(600):
             threading.Thread(target=http_start, args=(url_fin, proxies)).start()
-            
+
 except Exception as e:
     print(e)
     print("Error. Help: python3 seen.py <link> file <count>")
