f16d93d76e6995ca66487c5902c4075bca17d7fc
Jhovkvj <117099782+ikaayuxd@users.noreply.github.com>
2024-09-27 18:36:07 +0530

    Update seen.py

--- seen.py
+++ seen.py
@@ -5,6 +5,7 @@ import threading
 import psutil
 import os
 
+proxy_h = []
 def read_proxies_from_file(file_path):
     with open(file_path, 'r') as file:
         proxies = [line.strip() for line in file]
